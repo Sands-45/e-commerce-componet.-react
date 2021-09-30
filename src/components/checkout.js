f3ac24bf0e15935a9fd39e92d288e0a38c4df3aa
Sands-45 <sandsqa@hotmail.com>
2021-09-30 12:08:08 +0200

    Update

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import mastercard from "./img/mastercard.png";
 import paypal from "./img/paypal.png";
+import checkOut from "./img/check-out.svg";
 
 const Checkout = ({ items, discount, discount_percentage }) => {
   const tax = ((13 / 100) * items()).toFixed(2);
@@ -8,8 +9,11 @@ const Checkout = ({ items, discount, discount_percentage }) => {
   return (
     <div className="pt-28">
       <div className="grid grid-rows-6 w-[40%] h-[40rem] bg-gray-900 m-auto rounded-lg pt-3">
-        <div className="row-span-1 text-[#ffc300] text-center text-2xl uppercase font-bold">
-          Check-out
+        <div className="row-span-1 flex items-center justify-center">
+          <h2 className="text-[#ffc300] text-center text-2xl uppercase font-bold mr-2">
+            Check-out
+          </h2>
+          <img src={checkOut} alt="check-out" />
         </div>
         <div className="row-span-2 mx-2 p-3 rounded-md bg-[#4040412d]">
           <table className="text-white w-full">
@@ -38,19 +42,60 @@ const Checkout = ({ items, discount, discount_percentage }) => {
             Payment options:
           </h3>
           <div className="flex space-x-2">
-            <div className="w-12 h-10 bg-gray-800 flex items-center justify-center rounded-lg cursor-pointer hover:scale-95 transition duration-500 border">
+            <div className="w-12 h-10 bg-gray-800 flex items-center justify-center rounded-lg cursor-pointer hover:scale-95 transition duration-500 border border-[#ffc300]">
               <img className="h-[80%]" src={mastercard} alt="mastercard" />
             </div>
-            <div className="w-12 h-10 bg-gray-800 flex items-center justify-center rounded-lg cursor-pointer hover:scale-95 transition duration-500 border">
+            <div className="w-12 h-10 bg-gray-800 flex items-center justify-center rounded-lg cursor-pointer hover:scale-95 transition duration-500 border border">
               <img className="h-[80%]" src={paypal} alt="paypal" />
             </div>
           </div>
         </div>
-        <div className="row-span-2 m-auto"></div>
-        <div className="row-span-1 m-auto">
-          <button className="bg-[#ffc300] w-[100%] px-5 p-2 rounded-md hover:opacity-50 transition-opacity duration-500 font-bold text-center whitespace-nowrap">
-            Place Your Order
-          </button>
+        <div className="row-span-3 p-4 m-auto">
+          <form action="">
+            <div>
+              <label className="text-gray-400" htmlFor="name">
+                Card Holder Name
+              </label>
+              <input
+                className="block border border-gray-500 bg-transparent rounded-lg w-[100%] h-10 outline-none focus:border-blue-600 text-[#a8850f] px-2"
+                placeholder="Mr S Sands"
+              />
+            </div>
+            <div className="mt-2">
+              <label className="text-gray-400" htmlFor="name">
+                Card Number
+              </label>
+              <input
+                className="block border border-gray-500 bg-transparent rounded-lg w-[100%] h-10 outline-none focus:border-blue-600 text-[#a8850f] px-2"
+                placeholder="0000 0000 0000 0000"
+              />
+            </div>
+            <div className="mt-2 flex space-x-10">
+              <div className="w-[60%]">
+                <label className="text-gray-400" htmlFor="name">
+                  Expiration
+                </label>
+                <input
+                  className="block border border-gray-500 bg-transparent rounded-lg h-10 outline-none focus:border-blue-600 text-[#a8850f] px-2"
+                  placeholder="MM/YYYY"
+                />
+              </div>
+              <div className="w-[40%]">
+                <label className="text-gray-400" htmlFor="name">
+                  CVV
+                </label>
+                <input
+                  className="block border border-gray-500 bg-transparent rounded-lg h-10 outline-none w-[100%] focus:border-blue-600 text-[#a8850f] px-2"
+                  placeholder="837"
+                />
+              </div>
+            </div>
+            <div className="m-auto mt-3">
+              <button className="bg-[#ffc300] w-[100%] px-5 p-2 rounded-md hover:opacity-50 transition-opacity duration-500 font-bold text-center whitespace-nowrap">
+                Place Your Order
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
